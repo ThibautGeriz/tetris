f50aad25b73b01f134ba192474f51411ca666753
Thibaut Gery <thibaut.gery@datadoghq.com>
2020-12-25 18:51:09 +0100

    Add colors

--- tests/web.rs
+++ tests/web.rs
@@ -3,10 +3,11 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_tetris;
-use wasm_tetris::Square;
+use wasm_tetris::Color;
 use wasm_tetris::TetrisGame;
-use wasm_tetris::HEIGHT;
-use wasm_tetris::WIDTH;
+use wasm_tetris::Tetromino;
+use wasm_tetris::COLUMN_COUNT;
+use wasm_tetris::ROW_COUNT;
 
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
@@ -14,8 +15,8 @@ use wasm_bindgen_test::*;
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[cfg(test)]
-pub fn empty_game() -> Vec<Square> {
-    (0..200).map(|_i| Square::Free).collect()
+pub fn empty_game() -> Vec<Color> {
+    (0..200).map(|_i| Color::None).collect()
 }
 
 #[wasm_bindgen_test]
@@ -36,11 +37,8 @@ pub fn after_first_tick() {
     game.tick();
 
     // then
-    let first_line = &game.get_squares()[0..WIDTH];
-    let used_square_count = first_line
-        .iter()
-        .filter(|s| s == &&Square::Occupied)
-        .count();
+    let first_line = &game.get_squares()[0..COLUMN_COUNT];
+    let used_square_count = first_line.iter().filter(|s| s != &&Color::None).count();
     assert_eq!(used_square_count, 1);
 }
 
@@ -54,20 +52,14 @@ pub fn after_second_tick() {
     game.tick();
 
     // then
-    let first_line = &game.get_squares()[0..WIDTH];
-    let used_square_count = first_line
-        .iter()
-        .filter(|s| s == &&Square::Occupied)
-        .count();
+    let first_line = &game.get_squares()[0..COLUMN_COUNT];
+    let used_square_count = first_line.iter().filter(|s| s != &&Color::None).count();
     assert_eq!(used_square_count, 0);
 
-    let start_index: usize = WIDTH;
-    let end_index: usize = (WIDTH) * 2;
+    let start_index: usize = COLUMN_COUNT;
+    let end_index: usize = (COLUMN_COUNT) * 2;
     let second_line = &game.get_squares()[start_index..end_index];
-    let used_square_count = second_line
-        .iter()
-        .filter(|s| s == &&Square::Occupied)
-        .count();
+    let used_square_count = second_line.iter().filter(|s| s != &&Color::None).count();
     assert_eq!(used_square_count, 1);
 }
 
@@ -76,17 +68,17 @@ pub fn go_bottom() {
     // given
     let mut game = TetrisGame::new();
     game.tick();
-    let index = game.current_square.unwrap();
+    let index = game.get_tetromino().as_ref().unwrap().index;
 
     // when
     game.go_bottom();
 
     // then
-    let start_index: usize = WIDTH * (HEIGHT - 1);
-    let end_index: usize = WIDTH * HEIGHT;
+    let start_index: usize = COLUMN_COUNT * (ROW_COUNT - 1);
+    let end_index: usize = COLUMN_COUNT * ROW_COUNT;
     let last_line = &game.get_squares()[start_index..end_index];
-    let used_square_count = last_line.iter().filter(|s| s == &&Square::Occupied).count();
-    assert_eq!(game.current_square.unwrap(), index + 190);
+    let used_square_count = last_line.iter().filter(|s| s != &&Color::None).count();
+    assert_eq!(game.get_tetromino().as_ref().unwrap().index, index + 190);
     assert_eq!(used_square_count, 1);
 }
 
@@ -95,15 +87,16 @@ pub fn go_right() {
     // given
     let mut game = TetrisGame::new();
     game.tick();
-    let index = game.current_square.unwrap();
+    let index = game.get_tetromino().as_ref().unwrap().index;
 
     // when
     game.go_right();
 
     // then
-    assert_eq!(game.current_square.unwrap(), index + 1);
-    assert_eq!(game.get_squares()[index + 1], Square::Occupied);
-    assert_eq!(game.get_squares()[index], Square::Free);
+    let tetromino: &Tetromino = game.get_tetromino().as_ref().unwrap();
+    assert_eq!(tetromino.index, index + 1);
+    assert_eq!(game.get_squares()[index + 1], tetromino.color);
+    assert_eq!(game.get_squares()[index], Color::None);
 }
 
 #[wasm_bindgen_test]
@@ -111,13 +104,14 @@ pub fn go_left() {
     // given
     let mut game = TetrisGame::new();
     game.tick();
-    let index = game.current_square.unwrap();
+    let index = game.get_tetromino().as_ref().unwrap().index;
 
     // when
     game.go_left();
 
     // then
-    assert_eq!(game.current_square.unwrap(), index - 1);
-    assert_eq!(game.get_squares()[index - 1], Square::Occupied);
-    assert_eq!(game.get_squares()[index], Square::Free);
+    let tetromino: &Tetromino = game.get_tetromino().as_ref().unwrap();
+    assert_eq!(tetromino.index, index - 1);
+    assert_eq!(game.get_squares()[index - 1], tetromino.color);
+    assert_eq!(game.get_squares()[index], Color::None);
 }
