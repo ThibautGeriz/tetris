a0c01e8264c1ff95548c6bbda2727efddb1b2f94
ThibautGeriz <thibaut.gery@datadoghq.com>
2021-01-04 21:30:19 +0000

    Deploying to gh-pages from  @ 507fa913d540432d5ac56c1bb7e290d2a3c5f984 🚀

--- bootstrap.js
+++ bootstrap.js
@@ -219,7 +219,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/wasm_tetris_bg.wasm":"511e8deb2ce32e433bff"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/wasm_tetris_bg.wasm":"619a28d04b4b56e90f0d"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
